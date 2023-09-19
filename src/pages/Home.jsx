f140e044077bb3901b5bbf16f39befd26740273e
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-09-18 22:08:20 -0700

    added components and updated the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,39 @@
-import { AppBar, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Search } from '@mui/icons-material';
+import { Button, Typography, Toolbar, AppBar, Paper, TextField, Fab, Stack } from '@mui/material';
+import './Home.css';
+import Video from '../components/Video';
 
-export default function Home() {
-    return (
-        <>
-            <AppBar>
-                <Typography>
-                    Music YT
+
+function Home() {
+
+  return (
+    <>
+        <AppBar className='navbar'>
+            <Toolbar>
+                <Typography variant="h5" className='title'>
+                MusicYT
                 </Typography>
-            </AppBar>
-        </>
-    );
+                <Button color="inherit">Starred</Button>
+                <Button color="inherit">Settings</Button>
+                <Button color="inherit">About</Button>
+            </Toolbar>
+        </AppBar>
+
+        <Stack marginTop={10} marginBottom={12} alignItems='center'>
+            
+        </Stack>
+
+        <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+            <div className='inputGroup'>
+                <TextField variant='filled' label='enter your query' fullWidth/>
+                <Fab color='primary' className='floatingButton'>
+                    <Search/>
+                </Fab>
+            </div>
+        </Paper>
+    </>
+  );
 }
+
+export default Home;
