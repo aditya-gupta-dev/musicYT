8b1ebbdd5dd5f8461f2e6965083199afaff13fcd
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-09-22 02:26:21 -0400

    Update SavedVideo.jsx

--- src/components/SavedVideo.jsx
+++ src/components/SavedVideo.jsx
@@ -1,6 +1,12 @@
-export default function SavedVideo() {
+import { Typography } from "@mui/material";
+
+export default function SavedVideo({ video }) {
+    console.log("Component rendered");
     return (
         <>
+            <Typography>
+                { video.title }
+            </Typography>
         </>
     );
 }
