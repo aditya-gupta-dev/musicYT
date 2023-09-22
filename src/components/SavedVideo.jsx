70d3e6ff04b45c8a3a5dd39bb507de364572c3b7
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-09-22 02:38:17 -0400

    Update SavedVideo.jsx

--- src/components/SavedVideo.jsx
+++ src/components/SavedVideo.jsx
@@ -1,12 +1,29 @@
 import { Typography } from "@mui/material";
+import { titleSlice } from '../utils/utils';
 
 export default function SavedVideo({ video }) {
     console.log("Component rendered");
     return (
         <>
-            <Typography>
-                { video.title }
-            </Typography>
+            <Card sx={{ maxWidth: 345, minWidth: 345, marginTop: 4 }}>
+                <CardMedia
+                    sx={{ height: 140 }}
+                    image={video.thumbnailUrl}
+                    title="thumbnail"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {titleSlice(video.title)}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="medium" variant='outlined'>Listen</Button>
+                    <Button size="medium" variant='outlined'>
+                        <a href={`https://youtu.be/${video.id}`} style={{ textDecoration: 'none' }}>Watch</a>
+                    </Button>
+                    <Button size="medium" variant='outlined'>Remove</Button>
+                </CardActions>
+            </Card>
         </>
     );
 }
