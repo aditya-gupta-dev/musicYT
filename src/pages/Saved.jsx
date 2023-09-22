89d520fc06931cc34b06cfe9fdba2579572c9fb6
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-09-22 02:50:51 -0400

    Update Saved.jsx

--- src/pages/Saved.jsx
+++ src/pages/Saved.jsx
@@ -17,7 +17,7 @@ export default function Saved() {
         }
         setVideos(values);
     }, []);
-    
+
     return (
         <>
             <AppBar className='navbar'>
@@ -39,7 +39,7 @@ export default function Saved() {
             </AppBar>
 
             <Stack marginTop={10} marginBottom={18} alignItems="center" justifyContent="center">
-                { videos.map((vid, ind, arr) => <SavedVideo key={ind} video={vid}/>) }
+                { videos.map((vid, ind, arr) => <SavedVideo key={ind} video={vid} videos={videos} setVideos={setVideos}/>) }
             </Stack>
         </>
     );
